8909d4ad0a8bc6b8c70ed40468524c7a33262f3c
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-04-18 22:54:01 +0500

    Adjust styling of homepage cards

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,6 +1,6 @@
 async function retrevingdata() {
   const results = [];
-  for (let i = 1; i < 9; i += 1) {
+  for (let i = 1; i < 7; i += 1) {
     results.push(fetch(`https://api.tvmaze.com/shows/${i}`).then((res) => res.json()));
   }
   const dt = Promise.all(results);
@@ -11,7 +11,7 @@ const display = async () => {
   const container = document.querySelector('.cards');
   datas.forEach((cur) => {
     container.innerHTML += `
-  <div class="card">
+  <div id=${cur.id} class="card">
   <img src="${cur.image.medium}" alt="">
   <div class="likes"><h4>${cur.name}</h4> <span><span><i class="fa-regular fa-heart"></i></span><br>Likes</span></div>
   <button>Comments</button>
