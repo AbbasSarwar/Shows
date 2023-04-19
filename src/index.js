6d41814cb362c9714719121fb3a7ed219f2a069e
Abbas Sarwar <abbassarwar100@gmail.com>
2023-04-19 19:53:38 +0500

    LikesFunc updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import './styles.css';
 import retrevingdata from './modules/display.js';
+// import LikesAdd from './modules/likeCounts.js';
+// import getLikes from './modules/getLikes.js';
 
-fetch('https://api.tvmaze.com/shows/1');
-
-// https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/
-// https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/id/likes/
-// https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/id/comments/
-// https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/id/reservations/
-// /apps/abc234/comments?item_id=item1
-// /apps/abc234/reservations?item_id=item1
-retrevingdata();
+document.addEventListener('DOMContentLoaded', retrevingdata);
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,3 +1,6 @@
+import getLikes from './getLikes.js';
+import LikesAdd from './likeCounts.js';
+
 async function retrevingdata() {
   const results = [];
   for (let i = 1; i < 9; i += 1) {
@@ -6,6 +9,7 @@ async function retrevingdata() {
   const dt = Promise.all(results);
   return dt;
 }
+
 const display = async () => {
   const datas = await retrevingdata();
   const container = document.querySelector('.cards');
@@ -13,11 +17,19 @@ const display = async () => {
     container.innerHTML += `
   <div class="card">
   <img src="${cur.image.medium}" alt="">
-  <div class="likes"><h4>${cur.name}</h4> <span><span><i class="fa-regular fa-heart"></i></span><br>Likes</span></div>
+  <div class="likes"><h4>${cur.name}</h4> <span><i class="fa-regular fa-heart like-btn" data-id="${cur.id}"></i><br><span data-item-id="${cur.id}" class="add">Likes</span></span></div>
   <button>Comments</button>
   <button>Reservations</button>
 </div>
   `;
+    getLikes();
+    const likeBtn = document.querySelectorAll('.like-btn');
+    likeBtn.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const val = Number(e.target.dataset.id);
+        LikesAdd(val);
+      });
+    });
   });
 };
 display();
