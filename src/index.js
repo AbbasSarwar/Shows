4748b2f883685acd71792c89daf3f8304df78215
Abbas Sarwar <abbassarwar100@gmail.com>
2023-04-20 17:42:27 +0500

    Resolved conflicts

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import './styles.css';
 import retrevingdata from './modules/display.js';
-<<<<<<< HEAD
-// import popup from './modules/displaypopup.js';
 
-fetch('https://api.tvmaze.com/shows/1');
-
-// https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/
-// https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/id/likes/
-// https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/id/comments/
-// https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/id/reservations/
-// /apps/abc234/comments?item_id=item1
-// /apps/abc234/reservations?item_id=item1
 retrevingdata();
-// popup();
-=======
-// import LikesAdd from './modules/likeCounts.js';
-// import getLikes from './modules/getLikes.js';
-
 document.addEventListener('DOMContentLoaded', retrevingdata);
->>>>>>> feature/LikesFunc
